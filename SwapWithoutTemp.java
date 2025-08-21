62963443f3f646a67e90876593c6aaf4aff0fddc
Navdeep S <navdeep638095@gmail.com>
2025-08-21 09:29:13 +0530

    Add files via upload

---
+++ SwapWithoutTemp.java
@@ -0,0 +1,9 @@
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+        int a = 3, b = 2;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a+" "+b);
+    }
+}
