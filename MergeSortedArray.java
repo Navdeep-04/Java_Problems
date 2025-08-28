baa37a25ed1646672f1b7091fcb3d4a7c36d003a
Navdeep S <navdeep638095@gmail.com>
2025-08-28 21:01:56 +0530

    Add files via upload

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,23 +1,24 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class MergeSortedArray {
     public static void main(String[] args) {
         int[] nums1 = {1,2,3};
         int[] nums2 = {2,5,6};
+        ArrayList<Integer> arr = new ArrayList<>();
         int m = nums1.length;
         int n = nums2.length;
-        int[] arr = new int[m+n];
         for(int i = 0; i < m+n; i++)
         {
-            if(i < m){
-                arr[i] = nums1[i];
+            if(i < m && nums1[i] != 0){
+                arr.add(nums1[i]);
             }
-            else{
-                arr[i] = nums2[i-m];
+            if(i > m-1 && nums2[i-m] != 0){
+                arr.add(nums2[i-m]);
             }
         }
-        Arrays.sort(arr);
-        String string = Arrays.toString(arr);
-        System.out.println(string);
+        Collections.sort(arr);
+        System.out.println(arr);
     }
 }
